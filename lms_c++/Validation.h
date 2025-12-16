86868adfed9cb709486944e75b82c93bd7bd4880
awdmdi <awwadmehdikhan@gmail.com>
2025-12-16 13:23:44 +0500

    Add Validation.h with validation function declarations

---
+++ lms_c++/Validation.h
@@ -0,0 +1,11 @@
+#ifndef VALIDATION_H
+#define VALIDATION_H
+
+#include <string>
+using namespace std;
+
+bool isValidRoll(const string& r);
+bool isValidGmail(const string& g);
+bool isStrongPassword(const string& p);
+
+#endif
